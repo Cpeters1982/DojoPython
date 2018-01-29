012f4dd7a4c8a9a0b6635644b56b2901b07582c8
WallofYawn <indiekills@gmail.com>
2018-01-29 11:59:07 -0800

    Synced changes to animal_class.py

--- animal_class.py
+++ animal_class.py
@@ -8,18 +8,28 @@ class Animal(object):
     def run(self):
         self.health = self.health-5
         return self
-    def display_health(self):
-        print 'The animal is named {} and has {} health'.format(self.name, self.health)
+    def display_health(self, name):
+        print 'The animal is named {} and has {} health'.format(name, self.health)
         return self
 class Dog(Animal):
-    def set_health(self):
+    def __init__(self):
+        super(Dog, self).__init__(Dog, self)
         self.health = 150
-        return self
     def pet(self):
-        self.health = self.health+5
+        self.health += 5
+        return self
+class Dragon(Animal):
+    def __init__(self):
+        super(Dragon, self).__init__(Dragon, self)
+        self.health = 170
+        print 'I am a dragon'
+    def fly(self):
+        self.health -= 10
         return self
 
 # a = Animal('Shithead', 50)
 # print a.walk().walk().walk().run().run().display_health()
-d = Dog('Bagel', 150)
-print d.walk().walk().walk().run().run().pet().display_health()
+d = Dog()
+print d.walk().walk().walk().run().run().pet().display_health(name = 'Bagel')
+drago = Dragon()
+print drago.fly().display_health(name = 'Drago')
