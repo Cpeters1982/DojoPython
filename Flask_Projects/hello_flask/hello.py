b61441761236b1967400d5431322888d703b697f
WallofYawn <indiekills@gmail.com>
2018-01-29 13:58:47 -0800

    Updated hello.py to add another route and some light html with links to both routes

--- Flask_Projects/hello_flask/hello.py
+++ Flask_Projects/hello_flask/hello.py
@@ -1,12 +1,28 @@
+# pylint: disable=invalid-name
 '''
 Initial flask project.
 '''
-from flask import Flask  # Import Flask to allow us to create our app.
-app = Flask(__name__)    # Global variable __name__ tells Flask whether or not we are running the file
-                         # directly, or importing it as a module.
-@app.route('/')          # The "@" symbol designates a "decorator" which attaches the following
-                         # function to the '/' route. This means that whenever we send a request to
-                         # localhost:5000/ we will run the following "hello_world" function.
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
 def hello_world():
-  return 'Hello World!'  # Return the string 'Hello World!' as a response.
-app.run(debug=True)      # Run the app in debug mode.
+  '''
+  Defines a function hello_world, which points to index.html. Standard GET route.
+  '''
+  return render_template('index.html')
+
+@app.route('/success')
+
+def success():  
+  '''
+  Another GET route, points to success.html
+  '''
+  return render_template('success.html')
+
+app.run(debug=True)
+
+# Our route rules define what comes after the initial forward-slash. When a request matching our routing rule is received by the server, 
+# the function that immediately follows the @app.route('/some_route') is invoked.
