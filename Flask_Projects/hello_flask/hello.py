969927d5605c1e961d9d3c99fbf2d618005827e8
WallofYawn <indiekills@gmail.com>
2018-01-31 14:44:14 -0800

    Added advanced_routing app, altered hello_flask app

--- Flask_Projects/hello_flask/hello.py
+++ Flask_Projects/hello_flask/hello.py
@@ -3,25 +3,21 @@
 Initial flask project.
 '''
 from flask import Flask, render_template
-
 app = Flask(__name__)
-
 @app.route('/')
-
 def hello_world():
   '''
   Defines a function hello_world, which points to index.html. Standard GET route.
   '''
-  return render_template('index.html')
+  return render_template('index.html', name="Chris")
+# The name="SomeNameHere" parameter is a jinja test. Whatever we input here will be displayed on our index.html
 
 @app.route('/success')
-
 def success():  
   '''
   Another GET route, points to success.html
   '''
   return render_template('success.html')
-
 app.run(debug=True)
 
 # Our route rules define what comes after the initial forward-slash. When a request matching our routing rule is received by the server, 
